f9b80628d7030fdd41d000ce62303268a8aaf207
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-03-26 23:48:58 +0600

    add crispy & bootstrap about page

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,10 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #local
+    # 3rd party 
+    'crispy_forms',
+
+    # Local app
     'users',
     'pages',
 ]
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -134,6 +140,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 
 LOGIN_REDIRECT_URL = 'home'
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
+  """View for the home page"""
   template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+  """View for the about page"""
+  template_name = 'about.html'
