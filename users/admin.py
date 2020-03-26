056356b04f4f290d9c38de2a95b5afc7fde85fb9
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-03-26 17:15:00 +0600

    create pages app and home

--- users/admin.py
+++ users/admin.py
@@ -12,7 +12,7 @@ class CustomAdmin(UserAdmin):
   add_form = CustomUserCreationForm
   form = CustomUserChangeForm
   model = CustomUser
-  list_display = ['emai', 'username',]
+  list_display = ['email', 'username',]
 
 
 admin.site.register(CustomUser, CustomAdmin)
--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class CustomUser(AbstractUser):
   """CustomUser object"""
+  pass
