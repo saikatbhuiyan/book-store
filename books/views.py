ac0b48841383b931e37231e8e417a255204ee3f0
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-03-28 17:27:33 +0600

    add permissions

--- books/views.py
+++ books/views.py
@@ -1,16 +1,23 @@
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from .models import Book
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
   """List view for the book object"""
   model = Book
   context_object_name = 'book_list'
   template_name = 'books/book_list.html'
+  login_url = 'account_login'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(
+  LoginRequiredMixin,
+  PermissionRequiredMixin,
+  DetailView):
   """Detail view for the book object"""
   model = Book
   context_object_name = 'book'
   template_name = 'books/book_detail.html'
+  login_url = 'account_login'
+  permission_required = 'books.special_status'
